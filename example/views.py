6ea210edb391a38162333720550316eba37d7eb7
VictorMA18 <vmamanian@unsa.edu.pe>
2024-06-15 13:50:01 -0500

    Probando la biblioteca xhtml2pdf para poder convertir a un html en formato pdf para esto usamos render_to_pdf para poder hacer esto tambien creamos un .html el cual es una prueba para poder sacar el pdf de este documento

--- example/views.py
+++ example/views.py
@@ -1,3 +1,25 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.views.generic import View
+from django.template.loader import get_template
+from .utils import render_to_pdf
 # Create your views here.
+
+class GeneratePDF(View):
+    def get(self, request, *args, **kwargs):
+        template = get_template('invoice.html')
+        context = {
+            "invoice_number": 123,
+            "customer_name": "Victor Mamani",
+            "amount": 1399.9,
+            "date": "Today",
+        }
+        html = template.render(context)
+        pdf = render_to_pdf('invoice.html', context)
+        if pdf:
+            response = HttpResponse(pdf, content_type='application/pdf')
+            filename = "Invoice_%s" %("12345")
+            content = "inline; filename='%s'" %(filename)
+            response['Content-Disposition'] = content
+            return response
+        return HttpResponse("Not found")
