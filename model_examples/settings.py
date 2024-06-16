98560967871f55cff7639b3c92361b403db473f5
VictorMA18 <vmamanian@unsa.edu.pe>
2024-06-15 22:59:44 -0500

    Informe Final

--- model_examples/settings.py
+++ model_examples/settings.py
@@ -129,5 +129,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'mbvgjpv@gmail.com'
-EMAIL_HOST_PASSWORD = 'eqdndashimlsfnzg'
+EMAIL_HOST_USER = 'correo@gmail.com'
+EMAIL_HOST_PASSWORD = ''
